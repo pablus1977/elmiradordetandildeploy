bc7904c372ed4d52403eda696dfd0c1f60ef160e
Pablo Tumbarino <pablotumbarino@gmail.com>
2022-05-17 16:08:50 -0300

    corregido el carousel y los links a redes sociales

--- js/script.js
+++ js/script.js
@@ -1,35 +1,45 @@
-$(document).ready(function(){
-    $('.carousel').carousel({
-        interval: 3000
-    });
+$(document).ready(function () {
+  $('.carousel').carousel({
+    interval: 1000
+  });
 });
 
 
-$(document).ready(function(){
-  $.get("carousel.json", function(data) {
-      $("#car-in").html('<div class="carousel-item active"><img class="d-block w-100" src=\"' + data.fotos[0].ubicacion + '\" alt=\"' + data.fotos[0].alt + '\"></div>');
+$(document).ready(function () {
+  $.get("carousel.json", function (data) {
+    $("#car-in").html(`
+      <div class="carousel-item active">
+        <img class="d-block h-100 foto-carousel" src=\" ${data.fotos[0].ubicacion} \" alt=\" ${data.fotos[0].alt} \">
+        </div>
+    `);
 
-      $("#indic").html('<li data-target="#carousel" data-slide-to="0" class="active"></li>');
+    $("#indic").html('<li data-target="#carousel" data-slide-to="0" class="active"></li>');
 
-      for (let i in data.fotos) { 
-        if (i>0) {
-          $("#car-in").append('<div class="carousel-item"><img class="d-block w-100" src=\"' + data.fotos[i].ubicacion + '\" alt=\"' + data.fotos[i].alt + '\"></div>');
+    for (let i in data.fotos) {
+      if (i > 0) {
+        $("#car-in").append(`
+          
+        <div class="carousel-item">
+            <img class="d-block h-100 foto-carousel" src=\" ${data.fotos[i].ubicacion}\" alt=\" ${data.fotos[i].alt}\">
+            </div>
+          
+          `);
 
-          $("#indic").append('<li data-target="#carousel" data-slide-to=\"' + i + '\"></li>');
-        }
+        $("#indic").append('<li data-target="#carousel" data-slide-to=\"' + i + '\"></li>');
       }
+    }
 
   }, "json");
 });
 
 
-function fadereserva(){
-        $("#reserva").fadeOut(800);
-        $("#reserva").fadeIn(800);
-    }
+function fadereserva() {
+  $("#reserva").fadeOut(800);
+  $("#reserva").fadeIn(800);
+}
 
 
-$(document).ready(function(){
+$(document).ready(function () {
   window.setInterval(fadereserva, 1610);
 });
 
